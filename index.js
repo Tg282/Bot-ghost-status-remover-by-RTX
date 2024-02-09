7f8b4f156e225039574a9fcc8603d7c7b5512824
Khahgg <154500555+Khahgg@users.noreply.github.com>
2024-02-09 22:13:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching ISOJ","Listening to Isoj","Playing ISOJ"];
 
 
 let currentIndex = 0;
